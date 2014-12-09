14749953453478d2a669f48a872ec154ea116eba
Marie Hogebrandt <marie@pineberry.com>
2014-12-09 15:45:50 +0100

    Start of HTML validation

--- Assemblefile.js
+++ Assemblefile.js
@@ -2,19 +2,18 @@
 
 var assemble = require('assemble'),
 config = require('./config.js'),
-build = config.site.assemble.build,
+buildDir = config.pkg.config.dist,
 system = config.site.assemble.system,
-content = config.site.assemble.content,
-data = config.dataDir;
+content = config.site.assemble.content;
 
 // Load system
 assemble.layouts(system.root + '/' + system.layouts + '/**.hbs');
 assemble.helpers(system.root + '/' + system.helpers + '/**.js');
 assemble.partials(system.root + '/' + system.partials + '/**.hbs');
 
-assemble.option({layout: 'master'});
+assemble.option(config.site.assemble.options);
 
 assemble.task('default', function() {
   assemble.src(content.root + '/**.md')
-    .pipe(assemble.dest(build.root));
+    .pipe(assemble.dest(buildDir));
 });
--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var npmInfo = require('./package.json'),
 YAML = require('yamljs'),
 argv = require('yargs').argv,
@@ -5,41 +7,45 @@ dataDir = argv.dataDir || 'data',
 siteConfig =  YAML.load(dataDir + '/site.yaml');
 
 // Based on http://blog.codehatcher.com/node-js-alternate-config-file
-module.exports = function () {
-  console.log("Node Env Variable: " + process.env.NODE_ENV);
+module.exports = (function () {
+  console.log('Node Env Variable: ' + process.env.NODE_ENV);
 
   // istanbul ignore next: don't look at the env variables
   switch (process.env.NODE_ENV) {
     case null:
     case undefined:
-    case "local":
+    case 'local':
       return {
         env: 'local', //should be env/prod
         site: siteConfig,
-        dataDir: dataDir
+        dataDir: dataDir,
+        pkg: npmInfo
       };
-    case "dev":
-    case "development":
+    case 'dev':
+    case 'development':
       return {
         env: 'dev', //should be env/prod
         site: siteConfig,
-        dataDir: dataDir
+        dataDir: dataDir,
+        pkg: npmInfo
       };
-    case "test":
-    case "testing":
+    case 'test':
+    case 'testing':
       return {
         env: 'test', //should be env/prod, can be changed to prod when we are comfy with prod environ
         site: siteConfig,
-        dataDir: dataDir
+        dataDir: dataDir,
+        pkg: npmInfo
       };
-    case "prod":
-    case "production":
+    case 'prod':
+    case 'production':
       return {
         env: 'prod', //should be env/prod, can be changed to prod when we are comfy with prod environ
         site: siteConfig,
-        dataDir: dataDir
+        dataDir: dataDir,
+        pkg: npmInfo
       };
     default:
-      throw new Error("Environment Not Recognized");
+      throw new Error('Environment Not Recognized');
   }
-}();
+}());
